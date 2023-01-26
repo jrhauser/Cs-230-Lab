85e4ca8825e06d1b8f24037ac1e66e0b6b8c7e6b
jrhauser <jrhauser1127@gmail.comgit config --global user.name jrhausergit config --global user.email jrhauser1127@gmail.com>
2023-01-26 10:07:29 -0500

    start navbar

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Navbar1Component } from './navbar1/navbar1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Navbar1Component
   ],
   imports: [
     BrowserModule,
