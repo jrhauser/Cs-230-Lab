fcc620025c8348f9a50c9385a47c45e6455a5a0e
jrhauser <jrhauser1127@gmail.comgit config --global user.name jrhausergit config --global user.email jrhauser1127@gmail.com>
2023-02-16 10:36:52 -0500

    data binding cards

--- my-app/src/app/card1/card1.component.ts
+++ my-app/src/app/card1/card1.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card1',
   templateUrl: './card1.component.html',
   styleUrls: ['./card1.component.css']
 })
-export class Card1Component {
+export class Card1Component implements OnInit{
+  @Input() img: string;
+  @Input() imgDescription: string;
+  @Input() title: string;
+  @Input() description: string;
+  @Input() buttonText: string;
 
+  constructor() {
+    this.img ="No img found";
+    this.imgDescription = "No description";
+    this.title = "";
+    this.description = "";
+    this.buttonText = "";
+  }
+
+  ngOnInit(): void {
+  }
 }
