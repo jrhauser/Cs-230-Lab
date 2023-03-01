a142643fd69f806d60e2d83d98d86628e2ee840c
jrhauser <jrhauser1127@gmail.com>
2023-03-01 17:36:40 -0500

    fixed mocklist for the home-layout

--- my-app/src/app/card1/mock_list.ts
+++ my-app/src/app/card1/mock_list.ts
@@ -1,26 +1,26 @@
 export const mock_list = [
-    {
-        title: "DRY SUITS",
-        description: "Two decades of development have lead us to create dependable dry wear for the most demanding athletes. Combining durability with a performance inspired fit, our dry suits thrive in the harshest environments.",
-        buttonText: "Shop Dry Suits",
-        img: "not found",
-        imgDescription:"not found"
-    },
-    {
-        title: "Spray Skirts",
-        description: "The most trusted piece of gear in the industry. From class V expeditions to your backyard play runs, our rand and bungee skirts have you covered.",
-        buttonText: "Shop Spray Skirts",
-        img: "",
-        imgDescription:"not found"
-    },
-    {
-        title:"PERFORMANCE LAYERS",
-        description:"Our Thick Skin layering systems provide optimum warmth for the coldest conditions. Extremely soft 4-way stretch polyester fleece is form fitting for freedom of movement and a minimalist system under your gear. All the performance without the bulk.",
-        buttonText: "SHOP BASE LAYERS",
-        img: "not found",
-        imgDescription:"not found"
-    }, 
-    {
-        
-    },
-]
+  {
+    title: 'DRY SUITS',
+    description:
+      'Two decades of development have lead us to create dependable dry wear for the most demanding athletes. Combining durability with a performance inspired fit, our dry suits thrive in the harshest environments.',
+    buttonText: 'Shop Dry Suits',
+    img: 'not found',
+    imgDescription: 'not found',
+  },
+  {
+    title: 'Spray Skirts',
+    description:
+      'The most trusted piece of gear in the industry. From class V expeditions to your backyard play runs, our rand and bungee skirts have you covered.',
+    buttonText: 'Shop Spray Skirts',
+    img: '',
+    imgDescription: 'not found',
+  },
+  {
+    title: 'PERFORMANCE LAYERS',
+    description:
+      'Our Thick Skin layering systems provide optimum warmth for the coldest conditions. Extremely soft 4-way stretch polyester fleece is form fitting for freedom of movement and a minimalist system under your gear. All the performance without the bulk.',
+    buttonText: 'SHOP BASE LAYERS',
+    img: 'not found',
+    imgDescription: 'not found',
+  },
+];
