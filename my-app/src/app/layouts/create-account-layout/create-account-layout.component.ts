a6f0207e0048d0567c5f942c40f9678f1a879770
jrhauser <jrhauser1127@gmail.comgit config --global user.name jrhausergit config --global user.email jrhauser1127@gmail.com>
2023-04-26 17:24:54 -0400

    continue work on auth

--- my-app/src/app/layouts/create-account-layout/create-account-layout.component.ts
+++ my-app/src/app/layouts/create-account-layout/create-account-layout.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-create-account-layout',
   templateUrl: './create-account-layout.component.html',
   styleUrls: ['./create-account-layout.component.css']
 })
 export class CreateAccountLayoutComponent {
-  
+  email = "";
+  password = "";
+
+  constructor(private fireauth: AngularFireAuth,private router: Router) {}
+
+  createAccount() {
+    if (this.email == '') {
+      alert('please enter email');
+    }
+    if (this.password == '') {
+      alert('please enter password');
+    }
+    this.fireauth.createUserWithEmailAndPassword(this.email, this.password).then(
+      () => {
+        alert('Registered');
+        this.router.navigate(['/LoginLayout']);
+      },
+      (err) => {
+        alert(err.message);
+        this.router.navigate(['/CreateAccount']);
+      }
+    );
+  }
 }
